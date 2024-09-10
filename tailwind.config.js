c36b5915ec52b7f97fda7dabdeb29bdcdc6d15dc
sandeep74177 <ksandeep6983@gmail.com>
2024-09-10 10:33:32 +0530

    chat-AI using react tailwind and APIs

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Ensure this path includes your component files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
